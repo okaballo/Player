36c4c7175be925ab760c0a162e978abd390c67b9
okaballo <167494630+okaballo@users.noreply.github.com>
2024-12-07 13:24:24 +0200

    WhitePlayer.java

--- WhitePlayer.java
+++ WhitePlayer.java
@@ -1,89 +1,38 @@
-package com.chess.engine.pieces.Player;
+package com.chess.engine.Player;
 
-import com.chess.engine.Alliance;
+import com. chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.chess.engine.pieces.Piece;
-import com.chess.engine.board.BoardUtils;
-//import com.chess.engine.board.Move.KingSideCastleMove;
-//import com.chess.engine.board.Move.QueenSideCastleMove;
-//import com.chess.engine.pieces.Rook;
 
-
-
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
+public class WhitePlayer extends Player{
 
-public final class WhitePlayer extends Player {
+public WhitePlayer(Board board,
+                   Collection<Move> whiteStandardLegalMoves,
+                   Collection<Move> blackStandardLegalMoves) {
 
-    public WhitePlayer(final Board board,
-                       final Collection<Move> whiteStandardLegals,
-                       final Collection<Move> blackStandardLegals) {
-        super(board, whiteStandardLegals, blackStandardLegals);
-    }
+        super(board, whiteStandardLegalMoves, blackStandardLegalMoves);
+}
 
-    @Override
-    protected Collection<Move> calculateKingCastles(final Collection<Move> playerLegals,
-                                                    final Collection<Move> opponentLegals) {
+@Override
 
-        if(!hasCastleOpportunities()) {
-            return Collections.emptyList();
-        }
+public Collection<Piece> getActivePieces() {
 
-        final List<Move> kingCastles = new ArrayList<>();
+    return this.board.getWhitePieces();
+}
 
-        if(this.playerKing.isFirstMove() && this.playerKing.getPiecePosition() == 60 && !this.isInCheck()) {
-            //whites king side castle
-            if(this.board.getPiece(61) == null && this.board.getPiece(62) == null) {
-                final Piece kingSideRook = this.board.getPiece(63);
-                if(kingSideRook != null && kingSideRook.isFirstMove()) {
-                    if(Player.calculateAttacksOnTile(61, opponentLegals).isEmpty() &&
-                       Player.calculateAttacksOnTile(62, opponentLegals).isEmpty() &&
-                       kingSideRook.getPieceType() == ROOK) {
-                        if(!BoardUtils.isKingPawnTrap(this.board, this.playerKing, 52)) {
-                            kingCastles.add(new KingSideCastleMove(this.board, this.playerKing, 62, (Rook) kingSideRook, kingSideRook.getPiecePosition(), 61));
-                        }
-                    }
-                }
-            }
-            //whites queen side castle
-            if(this.board.getPiece(59) == null && this.board.getPiece(58) == null &&
-               this.board.getPiece(57) == null) {
-                final Piece queenSideRook = this.board.getPiece(56);
-                if(queenSideRook != null && queenSideRook.isFirstMove()) {
-                    if(Player.calculateAttacksOnTile(58, opponentLegals).isEmpty() &&
-                       Player.calculateAttacksOnTile(59, opponentLegals).isEmpty() && queenSideRook.getPieceType() == ROOK) {
-                        if(!BoardUtils.isKingPawnTrap(this.board, this.playerKing, 52)) {
-                            kingCastles.add(new QueenSideCastleMove(this.board, this.playerKing, 58, (Rook) queenSideRook, queenSideRook.getPiecePosition(), 59));
-                        }
-                    }
-                }
-            }
-        }
-        return Collections.unmodifiableList(kingCastles);
-    }
+@Override
 
-    @Override
-    public BlackPlayer getOpponent() {
-        return this.board.blackPlayer();
-    }
+public Alliance getAlliance() {
 
-    @Override
-    public Collection<Piece> getActivePieces() {
-        return this.board.getWhitePieces();
-    }
+    return Alliance.WHITE;
+}
 
-    @Override
-    public Alliance getAlliance() {
-        return Alliance.WHITE;
-    }
+@Override
+public Player getopponent() {
 
-    @Override
-    public String toString() {
-        return Alliance.WHITE.toString();
-    }
-
+    return this.board.blackPlayer();
+}
 }
