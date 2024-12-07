fdf76407371380817a37877878f71848c25252d4
okaballo <167494630+okaballo@users.noreply.github.com>
2024-12-07 13:23:30 +0200

    BlackPlayer.java

--- BlackPlayer.java
+++ BlackPlayer.java
@@ -1,93 +1,33 @@
-package com.chess.engine.pieces.Player;
+package com.chess.engine.Player;
 
 import com.chess.engine.Alliance;
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
 import com.chess.engine.pieces.Piece;
-import com.chess.engine.board.BoardUtils;
-//import com.chess.engine.board.Move.KingSideCastleMove;
-//import com.chess.engine.board.Move.QueenSideCastleMove;
-//import com.chess.engine.pieces.Rook;
-
-
-
-
-
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
 
-public final class BlackPlayer extends Player {
-
-    public BlackPlayer(final Board board,
-                       final Collection<Move> whiteStandardLegals,
-                       final Collection<Move> blackStandardLegals) {
-        super(board, blackStandardLegals, whiteStandardLegals);
-    }
-
-    @Override
-    protected Collection<Move> calculateKingCastles(final Collection<Move> playerLegals,
-                                                    final Collection<Move> opponentLegals) {
-
-        if (!hasCastleOpportunities()) {
-            return Collections.emptyList();
-        }
-
-        final List<Move> kingCastles = new ArrayList<>();
-
-        if (this.playerKing.isFirstMove() && this.playerKing.getPiecePosition() == 4 && !this.isInCheck) {
-            //blacks king side castle
-            if (this.board.getPiece(5) == null && this.board.getPiece(6) == null) {
-                final Piece kingSideRook = this.board.getPiece(7);
-                if (kingSideRook != null && kingSideRook.isFirstMove() &&
-                        Player.calculateAttacksOnTile(5, opponentLegals).isEmpty() &&
-                        Player.calculateAttacksOnTile(6, opponentLegals).isEmpty() &&
-                        kingSideRook.getPieceType() == ROOK) {
-                    if (!BoardUtils.isKingPawnTrap(this.board, this.playerKing, 12)) {
-                        kingCastles.add(
-                                new KingSideCastleMove(this.board, this.playerKing, 6, (Rook) kingSideRook, kingSideRook.getPiecePosition(), 5));
-
-                    }
-                }
-            }
-            //blacks queen side castle
-            if (this.board.getPiece(1) == null && this.board.getPiece(2) == null &&
-                    this.board.getPiece(3) == null) {
-                final Piece queenSideRook = this.board.getPiece(0);
-                if (queenSideRook != null && queenSideRook.isFirstMove() &&
-                        Player.calculateAttacksOnTile(2, opponentLegals).isEmpty() &&
-                        Player.calculateAttacksOnTile(3, opponentLegals).isEmpty() &&
-                        queenSideRook.getPieceType() == ROOK) {
-                    if (!BoardUtils.isKingPawnTrap(this.board, this.playerKing, 12)) {
-                        kingCastles.add(
-                                new QueenSideCastleMove(this.board, this.playerKing, 2, (Rook) queenSideRook, queenSideRook.getPiecePosition(), 3));
-                    }
-                }
-            }
-        }
-        return Collections.unmodifiableList(kingCastles);
-    }
-
-    @Override
-    public WhitePlayer getOpponent() {
-        return this.board.whitePlayer();
+public class BlackPlayer extends Player {
+    public BlackPlayer(Board board,
+                       Collection<Move> whiteStandardLegalMoves,
+                       Collection<Move> blackStandardLegalMoves) {
+    
+    super(board, blackStandardLegalMoves, whiteStandardLegalMoves);
+    
     }
 
     @Override
     public Collection<Piece> getActivePieces() {
         return this.board.getBlackPieces();
-    }
+    } 
 
     @Override
-    public Alliance getAlliance() {
+    public Alliance getAlliance(){
         return Alliance.BLACK;
     }
 
     @Override
-    public String toString() {
-        return Alliance.BLACK.toString();
-    }
-
+    public Player getopponent() {
+        return this.board.whitePlayer();
+        } 
 }
